d40462f15bfd9fecabf8c6cebb80a01dc24bc4d0
sanger16 <delgadodaniel.p@gmail.com>
2023-11-23 05:12:08 -0500

    Component with props

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,24 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-const App = () => {
-  const now = new Date()
-  const a = 10
-  const b = 20
-  console.log(now, a+b)
+const Hello = (props) => {
+  console.log(props);
   return (
-  <div>
-    <p>Hello World, it is { now.toString() }</p>
-    <p>
-      { a } plus { b } is { a + b }
-    </p>
-  </div>
+    <div>
+      <p>Hello { props.name }, you are { props.age } years old</p>
+    </div>
+  )
+}
+
+const App = () => {
+  const name = 'Peter';
+  const age = 10;
+  return (
+  <>
+    <h1>Greetings</h1>
+    <Hello name='Daniel' age={ 15 + 20 }/>
+    <Hello name={ name } age={ age }/>
+  </>
   )
 }
 
