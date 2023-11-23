4918a0452c070059767778d9f5b64f19709df409
sanger16 <delgadodaniel.p@gmail.com>
2023-11-23 05:20:03 -0500

    1-c Helper Functions and Destructuring

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-const Hello = (props) => {
-  console.log(props);
+const Hello = ({ name, age }) => {
+  {/* const { name, age } = props; */}
+  const bornYear = () => new Date().getFullYear() - age;
+ 
   return (
     <div>
-      <p>Hello { props.name }, you are { props.age } years old</p>
+      <p>Hello { name }, you are { age } years old</p>
+      <p>So you were probably born in { bornYear() }</p>
     </div>
   )
 }
