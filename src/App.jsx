cf81302ca05a3b3f914cc1ef39a4c76002107325
sanger16 <delgadodaniel.p@gmail.com>
2023-11-23 06:37:57 -0500

    1-d Conditional Rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,26 @@
 import { useState } from 'react'
 import './App.css'
 
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
+  }
+  return (
+    <div>
+      button press history: {props.allClicks.join(' ')}
+    </div>
+  )
+}
+
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
 
 const App = () => {
   const [clicks, setClicks] = useState({
@@ -34,15 +54,12 @@ const App = () => {
   return (
   <>
     { clicks.left }
-    <button onClick={ handleLeftClicks }>
-      left
-    </button>
+    <Button handleClick={handleLeftClicks} text='left' />
+    <Button handleClick={handleRightClicks} text='right' />
     { clicks.right }
-    <button onClick={ handleRightClicks }>
-      right
-    </button>
     <p>{allClicks.join(' ')}</p>
     <p>total {total}</p>
+    <History allClicks={allClicks} />
   </>
   )
 }
